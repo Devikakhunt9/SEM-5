64cdb6d384bb67def16260a37ca6939d8e83e6f9
Devikakhunt9 <104673812+Devikakhunt9@users.noreply.github.com>
2023-07-02 11:13:28 +0530

    Create Program.cs

---
+++ .net/Lab2/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.getDetails();
+            c1.bmi();
+
+            Class2 c2 = new Class2();
+            c2.getDetails();
+            c2.grade();
+
+            Class3 c3 = new Class3();
+            c3.prime();
+
+
+            Class4 c4 = new Class4();
+            c4.leapYear();
+
+            Class5 c5 = new Class5();
+            c5.fibbonaci();
+
+            Class6 c6 = new Class6();
+            c6.binary();
+
+            Class7 c7 = new Class7();
+            c7.permutation();
+        }
+    }
+}
