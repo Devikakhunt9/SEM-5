d3e26898c2c231bdb6a05608d639f3bb1f7b605c
Devikakhunt9 <104673812+Devikakhunt9@users.noreply.github.com>
2023-07-02 11:15:24 +0530

    Add files via upload

---
+++ .net/Lab1/Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Class2
+    {
+       static String n, c;
+        public Class2()
+        {
+           
+        }
+        public void getDetail()
+        {
+            Console.WriteLine("Enter Your Name :");
+            n = Console.ReadLine();
+            Console.WriteLine("Enter Your Contry Name :");
+            c = Console.ReadLine();
+
+        }
+        public void setDetail()
+        {
+            Console.WriteLine("Hello {0} from {1}", n, c);
+        }
+    }
+}
